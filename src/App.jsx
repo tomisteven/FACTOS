340fea383cbe19b9e063f5a38083c1fbd9b468fd
Tomas Steven Desktop <tomssteven22@gmail.com>
2025-02-13 20:11:45 -0300

    whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Works from "./components/Works";
 import Reviews from "./components/Reviews";
 import Services from "./components/Services";
 import Footer from "./components/Footer";
+import whatsApp from "./assets/whatsapp2.png";
+import arriba from "./assets/arriba.png";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -19,6 +21,18 @@ function App() {
       <Reviews />
       <Services />
       <Footer />
+      <button className="fixed bottom-5 left-5 p-3 bg-green-500 text-white rounded-full">
+        <img src={whatsApp} alt="" />
+      </button>
+
+      <button
+        className="fixed bottom-5 right-5 p-3 bg-blue-500 text-white rounded-full"
+        onClick={() => {
+          window.scrollTo(0, 0);
+        }}
+      >
+        <img src={arriba} alt="" />
+      </button>
     </div>
   );
 }
